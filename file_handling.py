28598ecdb108e9a231a58a9a1dae9d61eb3797e8
arsheenkour <arsheenkour47@gmail.com>
2022-12-03 18:46:09 +0530

    Create file_handling.py

---
+++ file_handling.py
@@ -0,0 +1,14 @@
+file1=open("myfile.txt","w")
+L= ["this is delhi \n","this is paris \n","this is london \n"]
+file1.write("hello \n")
+file1.writelines(L)
+file1.close()
+#appends adds at last
+file1 =open("myfile.txt","a")
+file1.write("today \n")
+file1.close()
+file1=open("myfile.txt","r")
+print("output of readlines after appending")
+print(file1.read())
+print()
+file1.close()
