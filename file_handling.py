bfbeedc7da4a893acd22cb0ffc5254afcc7300ea
arsheenkour <arsheenkour47@gmail.com>
2022-12-03 19:06:51 +0530

    Added a line
    Added a term

--- file_handling.py
+++ file_handling.py
@@ -1,4 +1,5 @@
-#Python code for file-handling
+#Python code for file-handling process
+# This is a beginner program
 file1=open("myfile.txt","w")
 L= ["this is delhi \n","this is paris \n","this is london \n"]
 file1.write("hello \n")
