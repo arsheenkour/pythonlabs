9993af0358605d11b20a04f6c3c1aafc75df3859
arsheenkour <arsheenkour47@gmail.com>
2022-12-06 15:31:34 +0530

    updated a file

--- pickling.py
+++ pickling.py
@@ -7,3 +7,14 @@ outfile.close()
 fst=open("animals",'rb')
 data = pickle.load(fst)
 print(data)
+ 
+ #2nd program 
+
+import pickle
+animalDict = {'Cat':2,'Dog':7 ,'Lion': 4 ,'Tiger':9,'leopard':11}
+outfile=open('animals','wb')
+pickle.dump(animalDict,outfile)
+outfile.close()
+fst=open("animals",'rb')
+data = pickle.load(fst)
+print(data)
